cfe4778621db8b4e54c4fb87e0b19c4062254948
Antoine LALA <a.lala@formation-cci12.fr>
2025-07-07 11:31:07 +0200

    ajout route /login en couirs

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
+import jwt
+import datetime
 from flasgger import Swagger
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from models.user import db, User
+from utils.jwt_utils import generate_jwt
+import os
+
+JWT_SECRET = os.environ.get("JWT_SECRET")
 
 app = Flask(__name__)
 swagger = Swagger(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    pseudo = data.get("pseudo")
+    password = data.get("password")
+    
+    if not pseudo or not password:
+        return jsonify({"error" : "pseudo ou password manquant"}), 400
+
+    user = User.query.filter_by(pseudo=pseudo).first()
+    
+    if not user:
+        return jsonify({"error" : "utilisateur inconnu"}), 404
+    
+    if not check_password_hash(user.password_hash, password):
+        return jsonify({"error": "Mot de passe incorrect"}), 401
+    
+    roles = User.query("roles").filter_by(pseudo=pseudo).first()
+    
+    token = generate_jwt(user.pseudo, roles)
+    
+    return jsonify({"token" : token}), 200
