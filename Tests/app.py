27a6a9d34e73085d142d3bc88f632a5bad3cffb4
juliendelclaux12 <delclauxj@3il.fr>
2025-07-07 12:00:07 +0200

    Ajout d'une méthode

--- Tests/app.py
+++ Tests/app.py
@@ -5,11 +5,32 @@ from flask import Flask
 app = Flask(__name__)
 swagger = Swagger(app)
 
-@app.route("/route", methods=["GET"])
+@app.route("/route", methods=["POST"])
 def get_route():
     
     """
-    Doc swagger ici
+        Description
+    ---
+    parameters:
+      - name: login
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            pseudo:
+              type: string
+              example: "Roger"
+            password:
+              type: string
+              example: "CoinCoin"
+    responses:
+      200:
+        description: "Ok"
+      401:
+        description: "Mot de passe incorrect"
+      404:
+        description: "Utilisateur Inconnu"
     """
     
     return "Retourne rien"
