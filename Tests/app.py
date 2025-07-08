cc9e17c76742078a89aa250d19a71a6600d25e6d
Sp3cTr <dev.theopottier@gmail.com>
2025-07-08 13:54:20 +0200

    tests flasgger login et register OK

--- Tests/app.py
+++ Tests/app.py
@@ -5,8 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 swagger = Swagger(app)
 
-@app.route("/route", methods=["POST"])
-def get_route():
+@app.route("/login", methods=["POST"])
+def post_login():
     
     """
         Description
@@ -34,5 +34,47 @@ def get_route():
     """
     
     return "Retourne rien"
+
+@app.route("/register", methods=["POST"])
+def post_register():
+    
+    """
+        Crée un nouvel utilisateur
+    ---
+    parameters:
+      - name: user_info
+        in: body
+        required: 
+          - pseudo
+          - email
+          - password
+        schema:
+          type: object
+          properties:
+            pseudo:
+              type: string
+              example: "Roger"
+            email:
+              type: string
+              example: "CoinCoin@duckdns.org"
+            password:
+              type: string
+              example: "pa_en!plastik"
+            avatar:
+              type: string
+              example: "super_duper_hot_duck.png"
+    responses:
+      201:
+        description: "Ok"
+      400:
+        description: "Champs obligatoires manquants"
+      409:
+        description: "Pseudo ou email déjà utilisé"
+      404:
+        description: "Utilisateur Inconnu"
+    """
+    
+    return "Retourne rien"
+
 
 app.run()
