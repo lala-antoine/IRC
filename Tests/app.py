5f8f5b0a9ccba79fefaf21fa37a73fa741aefe0a
Sp3cTr <dev.theopottier@gmail.com>
2025-07-08 14:30:32 +0200

    FIX : doc register et login

--- Tests/app.py
+++ Tests/app.py
@@ -9,28 +9,36 @@ swagger = Swagger(app)
 def post_login():
     
     """
-        Description
-    ---
-    parameters:
-      - name: login
-        in: body
-        required: true
-        schema:
-          type: object
-          properties:
-            pseudo:
-              type: string
-              example: "Roger"
-            password:
-              type: string
-              example: "CoinCoin"
-    responses:
-      200:
-        description: "Ok"
-      401:
-        description: "Mot de passe incorrect"
-      404:
-        description: "Utilisateur Inconnu"
+    Connecte l'utilisateur et renvoie un jeton JWT
+---
+produces:
+  - application/json
+parameters:
+  - name: login
+    in: body
+    required: true
+    schema:
+      type: object
+      properties:
+        pseudo:
+          type: string
+          example: "Roger"
+        password:
+          type: string
+          example: "CoinCoin"
+responses:
+  200:
+    description: "Connexion réussie, JWT retourné"
+    schema:
+      type: object
+      properties:
+        token:
+          type: string
+          example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+  401:
+    description: "Mot de passe incorrect"
+  404:
+    description: "Utilisateur Inconnu"
     """
     
     return "Retourne rien"
@@ -66,6 +74,29 @@ def post_register():
     responses:
       201:
         description: "Ok"
+        schema:
+          type: object
+          properties:
+            pseudo:
+              type: string
+              example: "Roger"
+            email:
+              type: string
+              example: "CoinCoin@duckdns.org"
+            password:
+              type: string
+              example: "pa_en!plastik"
+            avatar:
+              type: string
+              example: "super_duper_hot_duck.png"
+            statut:
+              type: string
+              example: "actif"
+            roles:
+              type: array
+              items:
+                types: string
+                example: ["user","..."]
       400:
         description: "Champs obligatoires manquants"
       409:
