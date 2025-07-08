31409c29891111302157bcb35195943260247055
Sp3cTr <dev.theopottier@gmail.com>
2025-07-08 14:49:11 +0200

    TESTS de whois

--- Tests/app.py
+++ Tests/app.py
@@ -108,4 +108,55 @@ def post_register():
     return "Retourne rien"
 
 
+@app.route("/whois/<pseudo>", methods=["GET"])
+def get_whois(pseudo):
+    
+    """
+    Récupère les informations de l'utilisateur connecté
+---
+parameters:
+  - name: pseudo
+    in: path
+    type: string
+    required: true
+    description: pseudo
+    example : Quack Sparrow
+responses:
+  200:
+    description: "Utilisateur trouvé"
+    schema:
+      type: object
+      properties:
+        pseudo:
+          type: string
+          example: "Roger"
+        cannaux:
+          type: array
+          items:
+            type: string
+          example: ["TODO"]
+        statut:
+          type: string
+          example: "en ligne"
+        derAct:
+          type: int
+          example: "11215568894543"
+        roles:
+          type: array
+          items:
+            type: string
+          example: ["admin", "moderateur"]
+  404:
+    description: "Utilisateur inconnu"
+    schema:
+      type: object
+      properties:
+        error:
+          type: string
+          example: "Utilisateur inconnu"
+
+    """
+    
+    return "Retourne rien"
+
 app.run()
